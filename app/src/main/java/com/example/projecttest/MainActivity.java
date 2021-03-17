4a2a590815ad6149f44caf0a94a635b68b3213e7
Rinad Jumaa <rinadjumaa72@gmail.com>
2021-03-17 16:17:02 +0200

    Version 2

--- app/src/main/java/com/example/projecttest/MainActivity.java
+++ app/src/main/java/com/example/projecttest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.projecttest;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -16,7 +15,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -29,7 +27,7 @@ import com.google.android.gms.tasks.Task;
 import java.io.IOException;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
     GoogleMap map;
     SearchView searchView;
     SupportMapFragment mapFragment;
@@ -41,12 +39,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        searchView = (SearchView) findViewById(R.id.location);
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         client = LocationServices.getFusedLocationProviderClient(this);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        getMyLocation();
         //getCurrentLocation();
+        getDesieredLocation();
 
+    }
 
+    private void getDesieredLocation() {
+        searchView = (SearchView) findViewById(R.id.location);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -72,52 +74,61 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 return false;
             }
         });
-        mapFragment.getMapAsync(this);
+       mapFragment.getMapAsync(new OnMapReadyCallback() {
+           @Override
+           public void onMapReady(GoogleMap googleMap) {
+               map = googleMap;
+           }
+       });
     }
 
-    private void getCurrentLocation() {
+    public void getMyLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, new String []{
+                    Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
             return;
         }
-        //Initialize task location
-        Task<Location> task= client.getLastLocation();
+        Task<Location> task = client.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
-            public void onSuccess(Location location) {
+            public void onSuccess(final Location location) {
                 if (location != null){
                     currentLocation = location;
-                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude() + "" + currentLocation.getLongitude(),
-                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude()
+                            + "---" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    System.out.println(currentLocation.getLatitude()
+                            + "---" + currentLocation.getLongitude());
+                    mapFragment.getMapAsync(MainActivity.this);
+
                 }
             }
-
         });
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions options = new MarkerOptions().position(latLng).title("Current Location");
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-        map.addMarker(options);
-        //mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
 
+        LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+        googleMap.addMarker(markerOptions);
+        //map = googleMap;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode){
             case REQUEST_CODE:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    getCurrentLocation();
-
+                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    getMyLocation();
                 }
                 break;
         }
+    }
 
-        }
+
     }
