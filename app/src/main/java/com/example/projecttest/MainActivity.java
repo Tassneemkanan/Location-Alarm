dd876d10975f45aff78d878b29d04ce5b6889c48
Tassneemkanan <39710176+Tassneemkanan@users.noreply.github.com>
2021-03-22 20:45:59 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/projecttest/MainActivity.java
+++ app/src/main/java/com/example/projecttest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.projecttest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -9,17 +11,25 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -27,34 +37,47 @@ import com.google.android.gms.tasks.Task;
 import java.io.IOException;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback ,
+        GoogleApiClient.ConnectionCallbacks ,
+        GoogleApiClient.OnConnectionFailedListener ,
+        LocationListener ,
+        GoogleMap.OnMarkerClickListener ,
+        GoogleMap.OnMarkerDragListener
+
+
+{
+    Button b_to;
+
     GoogleMap map;
     SearchView searchView;
     SupportMapFragment mapFragment;
     Location currentLocation;
-    FusedLocationProviderClient client; // to track current location
+    FusedLocationProviderClient client;
     private static final int REQUEST_CODE = 101;
+    double latitude , longitude ;
+    double end_latitude , end_longitude ;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        client = LocationServices.getFusedLocationProviderClient(this);
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        getMyLocation();
-        getDesieredLocation();
-
-    }
-
-    private void getDesieredLocation() {
         searchView = (SearchView) findViewById(R.id.location);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        b_to = findViewById(R.id.b_to);
+        //getCurrentLocation();
+
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 String location = searchView.getQuery().toString();
                 List<Address> addressList = null;
                 if (location != null || !location.equals("")) {
-                    //geocoder class helps adding a new marker on a new location
                     Geocoder geocoder = new Geocoder(MainActivity.this);
                     try {
                         addressList = geocoder.getFromLocationName(location, 1);
@@ -62,7 +85,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                         e.printStackTrace();
                     }
                     Address address = addressList.get(0);
-                    //adding the new marker
                     LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
                     map.addMarker(new MarkerOptions().position(latLng).title(location));
                     map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
@@ -75,68 +97,119 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 return false;
             }
         });
-        //I wrote new OnMapReadyCallback() to not get confused with the other method below
-       mapFragment.getMapAsync(new OnMapReadyCallback() {
-           @Override
-           public void onMapReady(GoogleMap googleMap) {
-               map = googleMap;
-           }
-       });
-    }
+        mapFragment.getMapAsync(this);
 
-    public void getMyLocation() {
-        // this if statement checks if permission to access location is accepted
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(this, new String []{
-                    Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+        }
+
+
+    private void getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             return;
         }
-        Task<Location> task = client.getLastLocation(); // get the last location the device was in (current location)
+        //Initialize task location
+        Task<Location> task= client.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
-            public void onSuccess(final Location location) {
+            public void onSuccess(Location location) {
                 if (location != null){
                     currentLocation = location;
-
-                    //show the latitude and longitude for the current location and display it as toast
-                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude()
-                            + "---" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-//                    System.out.println(currentLocation.getLatitude()
-//                            + "---" + currentLocation.getLongitude());
-                    // get the map when everything is ready
-                    mapFragment.getMapAsync(MainActivity.this);
-
+                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude() + "" + currentLocation.getLongitude(),
+                            Toast.LENGTH_SHORT).show();
                 }
             }
+
         });
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions options = new MarkerOptions().position(latLng).title("Current Location");
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        map.addMarker(options);
+        //mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setOnMarkerClickListener(this);
+        map.setOnMarkerDragListener(this);
 
-        LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()); // new latlng with the current location
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here");
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-        googleMap.addMarker(markerOptions); //add the marker
-        //map = googleMap;
     }
 
-
-    // this method checks if the permission is granted then call getmylocation method
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode){
             case REQUEST_CODE:
-                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
-                    getMyLocation();
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getCurrentLocation();
+
                 }
                 break;
         }
-    }
+
+        }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
     }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.setDraggable(true);
+        return false;
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+end_latitude=marker.getPosition().latitude;
+end_longitude=marker.getPosition().longitude;
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
+
+    public void OnClick(View view) {
+        b_to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ///    int b_to = R.id.b_to;
+                map.clear();
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(new LatLng(end_latitude,end_longitude));
+                markerOptions.title("Destination");
+                /// markerOptions.draggable(true);
+
+                float result[]=new float[10];
+                Location.distanceBetween(latitude,longitude,end_latitude,end_longitude,result);
+                markerOptions.snippet("Distance = " +result[0]);
+                map.addMarker(markerOptions);
+            }
+        });
+    }
+}
